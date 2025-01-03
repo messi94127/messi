e5bf3ac9d87b759786f1c97e3c91b41404b995ae
messi94127 <smessi94127@icloud.com>
2025-01-03 23:57:05 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -534,6 +534,7 @@ class ClearEnemiesItem extends GameObject {
         element.style.bottom = `${y}px`;
         enemyContainer.appendChild(element);
         super(x, y, 30, 30, element); // サイズを小さめに設定
+        this.scoreValue = 500; // この敵を倒したときに加算するスコア
     }
 
     move() {
